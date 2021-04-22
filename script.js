5a3450f359fed266f5632e50265ffdb3c9f4e238
Leninner <Mazabandalenin180@gmail.com>
2021-04-21 20:27:38 -0500

    Agregamos interactividad a  la elección del computador y la persona. Queda aun por agregar interactividad al resultado de ronda y resultado final

--- script.js
+++ script.js
@@ -1,63 +1,68 @@
 // Declaramos variables
-const pares = ["Rock", "Paper", "Scissors"];
+const pares = new Array();
+pares[0] = "sources/paper.png"
+pares[1] = "sources/rock.png";
+pares[2] = "sources/scissors.png";
 let contHumano = 0;
 let contPc = 0;
 
 
-// Función para jugar la ronda
-// Tenemos las comparativas con el tipo == "value". Esto
-// si se puede hacer. OJO!
+//Eventos de Escucha
 
-function playRound(computerSelection, playerSelection){
-  computerSelection = computerPlay();
-  if(computerSelection == playerSelection){
-    alert("Es un empate claro");
-  } else if(computerSelection == "papper"){
-      if(playerSelection == "scissors"){
-        alert("El ganador es el humano");
-        contHumano++;
-      } else if(playerSelection == "rock"){
-        alert("El ganador es la computadora");
-        contPc++;
-      }
-  } else if(computerSelection == "rock"){
-    if(playerSelection == "scissors"){
-      alert("El ganador es la computadora");
-      contPc++;
-    } else if(playerSelection == "papper"){
-      alert("El ganador es el humano");
-      contHumano++;
-    }
-  } else if(computerSelection == "scissors"){
-    if(playerSelection == "papper"){
-      alert("El ganador es la computadora");
-      contPc++;
-    } else if(playerSelection == "rock"){
-      alert("El ganador es el humano");
-      contHumano++;
-    }
-  }
+const papelito = document.querySelector('#papel');
+papelito.addEventListener('click', escucha);
 
-  if(contPc == 5){
-    alert("EL GRANDIOSO GANADOR ES LA PC!")
-  } else if(contHumano == 5){
-    alert("EL GRANDIOSO GANADOR ES EL HUMANO!");
-  }
+const roquita = document.querySelector('#roca');
+roquita.addEventListener('click', escucha);
+
+const tijerita = document.querySelector('#tijeras');
+tijerita.addEventListener('click', escucha);
+
+
+//Código nuevo
+
+const div = document.querySelector('#humano');
+const img = document.createElement('img');
+const divPC = document.querySelector('#pc');
+const imgPC = document.createElement('img');
+const hResult = document.querySelector('#separResult');
+const h  = document.createElement('h3');
+
+function escucha(e){
+  let source = e.path[0].src; 
+  img.src = source;
+  img.style.display = 'flex';
+  img.style.marginLeft = "105px";
+  img.style.marginTop = '75px';
+  div.appendChild(img);
+  console.log(source);
+  
+  imgPC.src = computerPlay();
+  imgPC.style.display = 'flex';
+  imgPC.style.marginLeft = "105px";
+  imgPC.style.marginTop = '75px';
+  divPC.appendChild(imgPC);
+  console.log(imgPC.src);
+  playRound();
 }
 
-
-
+function playRound(){
+  let ganador;
+  if(img.src === imgPC.src){
+    ganador = "EMPATE";
+  }
+  h.textContent = "El ganador de esta ronda: " + ganador;
+  h.style.paddingTop = "25px";
+  hResult.appendChild(h);
+}
 
 // Función para que la computadora haga su elección
 
 function computerPlay(){
   let ha = Math.floor(Math.random() * pares.length);
-  let loop = pares[ha].toLowerCase();
+  let loop = pares[ha];
   return loop;
 }
 
-//Código nuevo
-const papelito = document.querySelector('#papel');
-papelito.addEventListener('click', () => {
-  alert("Hola");
-});
+
+

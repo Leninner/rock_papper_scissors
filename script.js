3fa313c0e2f475fe8a95e6b0349f51f9aa8955b9
Leninner <Mazabandalenin180@gmail.com>
2021-04-22 23:24:45 -0500

    Finalizamos el juego de piedra, papel o tijera. Quizá más adelante, haga algunos cambios, pero mientras tanto, se queda así

--- script.js
+++ script.js
@@ -21,12 +21,16 @@ tijerita.addEventListener('click', escucha);
 
 //Código nuevo
 
+//Traemos el elemento de HTML con selectores de consulta
+// y creamos un nuevo elemento
 const div = document.querySelector('#humano');
 const img = document.createElement('img');
 const divPC = document.querySelector('#pc');
 const imgPC = document.createElement('img');
 const hResult = document.querySelector('#separResult');
 const h  = document.createElement('h3');
+const resultHumano = document.querySelector('#resultHumano');
+const resultPC = document.querySelector('#resultPC');
 
 function escucha(e){
   let source = e.path[0].src; 
@@ -34,6 +38,7 @@ function escucha(e){
   img.style.display = 'flex';
   img.style.marginLeft = "105px";
   img.style.marginTop = '75px';
+  //Agregamos la imagen del humano a la caja de selección humano
   div.appendChild(img);
   console.log(source);
   
@@ -41,19 +46,60 @@ function escucha(e){
   imgPC.style.display = 'flex';
   imgPC.style.marginLeft = "105px";
   imgPC.style.marginTop = '75px';
+  //Agregamos la imagen de la PC a la caja de selección PC
   divPC.appendChild(imgPC);
   console.log(imgPC.src);
   playRound();
 }
 
+//Funcion para jugar rondas
 function playRound(){
   let ganador;
   if(img.src === imgPC.src){
     ganador = "EMPATE";
   }
+  else if(imgPC.src == 'http://127.0.0.1:5500/sources/paper.png'){
+    if(img.src == 'http://127.0.0.1:5500/sources/scissors.png'){
+      ganador = 'ERES TÚ!';
+      contHumano++;
+    }
+    else if(img.src == 'http://127.0.0.1:5500/sources/rock.png'){
+      ganador = 'ES LA PC!';
+      contPc++;
+    }
+  }
+  else if(imgPC.src == 'http://127.0.0.1:5500/sources/rock.png'){
+    if(img.src == 'http://127.0.0.1:5500/sources/scissors.png'){
+      ganador = "ES LA PC!";
+      contPc++;
+    }
+    else if(img.src == 'http://127.0.0.1:5500/sources/paper.png'){
+      ganador = "ERES TÚ!";
+      contHumano++;
+    }
+  }
+  else if(imgPC.src == 'http://127.0.0.1:5500/sources/scissors.png'){
+    if(img.src == 'http://127.0.0.1:5500/sources/paper.png'){
+      ganador = "ES LA PC!";
+      contPc++;
+    }
+    else if(img.src == 'http://127.0.0.1:5500/sources/rock.png'){
+      ganador = "ERES TÚ!";
+      contHumano++;
+    }
+  }
   h.textContent = "El ganador de esta ronda: " + ganador;
   h.style.paddingTop = "25px";
+  //Agregamos el resultado a la caja de resultado
   hResult.appendChild(h);
+  resultHumano.textContent = contHumano;
+  resultPC.textContent = contPc;
+  if(contPc == 5){ 
+    alert('GANÓ LA COMPUTADORA');
+  }
+  else if(contHumano == 5){
+    alert('GANASTE TÚ!');
+  }
 }
 
 // Función para que la computadora haga su elección
